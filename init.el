a080b709191156399c6ca5d7fd88a38904bae9c1
snlansky <snlan@live.cn>
2019-03-31 23:10:04 +0800

    up

--- init.el
+++ init.el
@@ -3,11 +3,35 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;;(package-initialize)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages") t)
+  )
+
+;; add common lisp
+(require 'cl)
+;; add whatever package you want here
+(defvar snlan/packages '(
+			 company
+			 monokai-theme
+			 ) "Default packages")
+(defun snlan/packages-installed-p ()
+  (loop for pkg in snlan/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+(unless (snlan/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg snlan/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(electric-indent-mode -1)
+
 (setq inhibit-splash-screen t)
 
 (linum-mode t)
@@ -20,6 +44,23 @@
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 (setq-default cursor-type 'bar)
 (setq make-backup-files nil)
+
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(delete-selection-mode t)
+
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(global-hl-line-mode t)
+
 (global-company-mode t)
 
 (custom-set-variables
@@ -27,6 +68,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default)))
  '(package-selected-packages (quote (company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -34,3 +78,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; package source
+
