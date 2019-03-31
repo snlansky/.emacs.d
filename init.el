3d3f68720f23cb2f573c96eb2658aa46e4050d49
snlansky <snlan@live.cn>
2019-03-31 21:55:04 +0800

    fisrt commit

---
+++ init.el
@@ -0,0 +1,36 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(electric-indent-mode -1)
+(setq inhibit-splash-screen t)
+
+(linum-mode t)
+(global-linum-mode t)
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+(setq-default cursor-type 'bar)
+(setq make-backup-files nil)
+(global-company-mode t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
